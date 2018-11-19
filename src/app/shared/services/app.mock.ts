46bc4eff1c8293f8b8edef7af3f4b5ff21f75239
aolite <aitorcorchero@gmail.com>
2018-11-19 21:18:49 +0100

    image links correction

--- src/app/shared/services/app.mock.ts
+++ src/app/shared/services/app.mock.ts
@@ -6,7 +6,7 @@ export const APP_VAR: APP [] = [
         id: 1,
         title: 'Rioter Semantic Stream Engine',
         description: 'Detection of water critical events through RDF Stream processing Engines.',
-        image: '../../assets/img/rsp.jpg',
+        image: 'assets/img/rsp.jpg',
         type: 'Web Tool',
         url: ''
     },
@@ -14,7 +14,7 @@ export const APP_VAR: APP [] = [
         id: 2,
         title: 'Rioter Exploration Engine',
         description: 'Visualise and explore water related information',
-        image: '../../assets/img/eye.jpg',
+        image: 'assets/img/eye.jpg',
         type: 'Web Tool',
         url: ''
     },
